148cb5c6d5ccb9565fc03579d3690f4a9739723f
rayankhan01 <rk12172002@gmail.com>
2024-03-09 20:32:55 +0500

    Guest List

---
+++ main.js
@@ -0,0 +1,4 @@
+var guests = ["Salman", "Maqsoom", "Nubair"];
+guests.forEach(function (guests) {
+    console.log("Dear ".concat(guests, ",would you like to join me for dinner?"));
+});
---
+++ main.ts
@@ -0,0 +1,3 @@
+let guests: string[] = ["Salman" , "Maqsoom" , "Nubair"]
+guests.forEach(guests => {console.log(`Dear ${guests},would you like to join me for dinner?`);
+});
